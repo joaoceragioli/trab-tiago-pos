8492fdaaf143316737b80d595be3fcd40cc9cbbf
Your Name <you@example.com>
2022-05-12 19:53:12 -0300

    API INICIAL BONITA

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/')
+def helloworld():
+    return "API LINDA!"
+
+@app.route('/macaco')
+def monkeyday():
+    return render_template('index.html')
+
+@app.route('/<int:number>/')
+def incrementer(number):
+    return "Incremented number is " + str(number+1)
+    
+@app.route('/<string:name>/')
+def hello(name):
+    return "Hello " + name
+
+if __name__ == '__main__':
+    app.run(debug=True)
