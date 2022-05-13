dd9157c709776675428a8790a5af72a5a1b1c0dd
Gabriel Soares Campanário <gabriel.camp96@hotmail.com>
2022-05-12 22:24:24 -0300

    frontend working

--- server.js
+++ server.js
@@ -8,21 +8,19 @@ function submit() {
     
     // Seta paramêtros da requisição e envia a requisição
     let post = JSON.stringify({
-        text: "bigas"
+        text: document.getElementsByName('review')[0].value
     })
 
     ajax.send(post);
     
     // Cria um evento para receber o retorno.
     ajax.onreadystatechange = function() {
-      
       // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
         if (ajax.readyState == 4 && ajax.status == 200) {
         
             var data = ajax.responseText;
             
-        // Retorno do Ajax
-            console.log(data);
+            document.getElementById('result').textContent = JSON.parse(data).text
         }
     }
 }
