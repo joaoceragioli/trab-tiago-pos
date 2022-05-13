caf2f2373104ad43d5931739d95f10b137dec7d0
Your Name <you@example.com>
2022-05-12 21:14:30 -0300

    corrigindo cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def helloworld():
@@ -11,6 +14,7 @@ def monkeyday():
     return render_template('index.html')
 
 @app.route('/ocrdobigas', methods=["POST"])
+@cross_origin()
 def reviewpost():
      input_json = request.get_json(force=True) 
      dictToReturn = {'text':input_json['text']}
